9e48ae5ba32ad7f1855afc582ba8878cf0369688
thgendry <thisanji@gmail.com>
2020-08-30 15:56:10 -0300

    Desafio 05 finalizado

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,29 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    if (type === 'income') {
+      const transaction = this.transactionsRepository.create({
+        title,
+        value,
+        type,
+      });
+      return transaction;
+    } else if (type === 'outcome') {
+      const balance = this.transactionsRepository.getBalance();
+      if (balance.total >= value) {
+        const transaction = this.transactionsRepository.create({
+          title,
+          value,
+          type,
+        });
+        return transaction;
+      } else {
+        throw Error('Balance below outcome value, transaction denied');
+      }
+    } else {
+      throw Error('Transaction type not allowed (income | outcome)');
+    }
   }
 }
 
